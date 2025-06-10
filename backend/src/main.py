e0a21c05a7b3948c90ac4a4b197ec47d43922d00
aFrattini <antonellafrattini@hotmail.com>
2025-06-10 23:20:06 +0200

    fix: habilitar CORS para frontend

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,13 +2,18 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from src.routers.titanicRouter import router as TitanicRouter
 
+
 app = FastAPI()
+
+# Permitir solo tu frontend de Render
+origins = [
+    "https://antonellafrattiniporfolio.onrender.com"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://antonellafrattiniporfolio.onrender.com/"],
-    allow_methods=["*"],
-    allow_headers=["*"]
+    allow_origins=origins,            # Dominios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],              # Métodos HTTP permitidos
+    allow_headers=["*"],              # Encabezados permitidos
 )
-
-app.include_router(TitanicRouter)
