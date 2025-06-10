3af3480a70c3122913d469f5b70b1d4b6dc6217e
aFrattini <antonellafrattini@hotmail.com>
2025-06-10 20:52:47 +0200

    fix: actualiza import de routers

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from routers.titanicRouter import router as TitanicRouter
+from src.routers.titanicRouter import router as TitanicRouter
 
 app = FastAPI()
 
