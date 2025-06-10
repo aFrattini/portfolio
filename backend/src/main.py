a97df5b9c7553e6646f99a678c16ac487f981999
aFrattini <antonellafrattini@hotmail.com>
2025-06-10 19:07:36 +0200

    Actualiza main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from routers.titanicRouter import router as TitanicRouter
+from src.routers.titanicRouter import router as TitanicRouter
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://antonellafrattiniporfolio.onrender.com/"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
